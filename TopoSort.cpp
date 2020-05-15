9f3f8fe8d5bb08aa048dc99513b1e967a8e99c65
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2020-05-14 22:14:39 -0300

    Update TopoSort.cpp

--- TopoSort.cpp
+++ TopoSort.cpp
@@ -1,22 +1,25 @@
-void topoSort(Grafo& grafo){
+bool toposort(vector <vector <int>>& grafo) {
     vector<int> lista;
-    int v_size=v_number(grafo);
-    for(int i=0;i<v_size;i++) if(grafo.grau[i]==0) lista.pb(i);
-    int ini=0;
-    while(ini<lista.size()){
-        int atual= lista[ini];
+    for(int i = 0; i <grafo.size(); i++) if(grau[i] == 0) lista.pb(i);
+    int ini = 0;
+    while(ini < lista.size()){
+        int atual = lista[ini];
         ini++;
-        for(int i=0;i<grafo.lista[atual].size();i++){
-            int v=grafo.lista[atual][i].first;
-            grafo.grau[v]--;
-            if(grafo.grau[v]==0) lista.push_back(v);
+        for(int i = 0; i < grafo[atual].size(); i++){
+            int v = grafo[atual][i];
+            grau[v]--;
+            if(grau[v]==0){
+                lista.push_back(v);  
+            } 
         }
     }
-    if(lista.size()<v_number(grafo)) cout << "Deu merda" << endl;
+    if(lista.size() < grafo.size()) return false;
     else{
-        for(int i=0;i<v_size;i++){
-            cout << lista[i] << " ";
+        for(int i = 0; i < grafo.size(); i++){
+            if(i == 0)cout << lista[i];
+            else cout << ' ' << lista[i];
         }
         cout << endl;
     }
+    return true;
 }
